afb9f3e90ecd6259f6bff3c6f5272981127a9a3c
Sricharan9111 <157402311+Sricharan9111@users.noreply.github.com>
2024-03-04 17:58:03 -0500

    Update product_of_array_except_itself.py

--- product_of_array_except_itself.py
+++ product_of_array_except_itself.py
@@ -1,43 +1,11 @@
-def add(x, y):
-    return x + y
+def factorial(num) {
+    if (num == 0) {
+        return 1
+    } else {
+        return num * factorial(num - 1)
+    }
+}
 
-def subtract(x, y):
-    return x - y
-
-def multiply(x, y):
-    return x * y
-
-def divide(x, y):
-    if y == 0:
-        return "Cannot divide by zero!"
-    else:
-        return x / y
-
-print("Select operation:")
-print("1. Add")
-print("2. Subtract")
-print("3. Multiply")
-print("4. Divide")
-
-while True:
-    choice = input("Enter choice (1/2/3/4): ")
-
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-
-        if choice == '1':
-            print("Result:", add(num1, num2))
-        elif choice == '2':
-            print("Result:", subtract(num1, num2))
-        elif choice == '3':
-            print("Result:", multiply(num1, num2))
-        elif choice == '4':
-            print("Result:", divide(num1, num2))
-    else:
-        print("Invalid Input")
-
-    again = input("Do you want to perform another calculation? (yes/no): ")
-    if again.lower() != 'yes':
-        break
-
+def number = 5
+def result = factorial(number)
+println("Factorial of $number is: $result")
